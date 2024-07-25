0206558273960ae6ada3b7a20e53acfb2629404e
Harshit Rajpurohit <115687640+Harshitrajpurohit@users.noreply.github.com>
2024-07-26 02:35:43 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,13 @@
-const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true
-      },
-      password: {
-        type: String,
-        required: true
-      }
-})
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+});
 
-const User = mongoose.model("User",userSchema);
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
